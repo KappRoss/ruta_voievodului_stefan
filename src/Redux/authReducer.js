d917303cce2d67663868ebe59fb6916d44fa58a4
KappRoss <kaplicnyj777@gmail.com>
2020-05-29 21:46:53 +0300

    fix

--- src/Redux/authReducer.js
+++ src/Redux/authReducer.js
@@ -26,6 +26,7 @@ export const setAuthUserData = (firstName, lastName, email, message) => ({type:S
 export const login = (firstName, lastName, email, message) => async (dispath) => {
     let response = await authAPI.login(firstName, lastName, email, message)
         if (response.data.resultCode === 0){
+            dispath(setAuthUserData());
             let message = response.data.messages.length > 0 ? response.data.messages[0] : "some error"; 
             dispath(stopSubmit("login", {_error: message}))
         } 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+import store from './Redux/redux-store';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+let rerenderEntireTree = () => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <Provider store = {store}>
+                <App />
+            </Provider>
+        </BrowserRouter>, document.getElementById('root'));
+}
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+rerenderEntireTree(store.getState())
+
 serviceWorker.unregister();
